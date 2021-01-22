df182a7ce7b0380566db7b5c30d7dbe6be30a374
Raj Chowdhury <30806882+Rajchowdhury420@users.noreply.github.com>
2021-01-23 01:37:13 +0530

    it'll look nice !

--- controllers/error.js
+++ controllers/error.js
@@ -4,3 +4,12 @@ exports.getErrorPage = (req, res, next) => {
       path:"/404" 
     });
   };
+
+
+/*
+Add the following tag in your 404 page
+
+<img alt="404" 
+     src="https://illustatus.herokuapp.com/?title=Oops,%20Page%20not%20found&fill=%234f86ed"/>
+     
+*/
